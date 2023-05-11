75da0ef7fd33a4f156ec64724d8ba684b2470a9b
aperros200 <133211782+aperros200@users.noreply.github.com>
2023-05-11 11:45:24 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+let DISTANCIA = maqueen.Ultrasonic(PingUnit.Centimeters)
 basic.forever(function () {
-	
+    DISTANCIA = maqueen.Ultrasonic(PingUnit.Centimeters)
+    if (DISTANCIA > 0 && DISTANCIA < 10) {
+        maqueen.servoRun(maqueen.Servos.S1, 102)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+    }
 })
